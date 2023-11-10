8bc23816bfabce0370ae4b5490481acaeb48ca82
Pablo Alves <74788322+aquelepablo@users.noreply.github.com>
2023-11-10 09:37:11 +0000

    Change getComputerChoice to return int

--- js/script.js
+++ js/script.js
@@ -13,20 +13,25 @@ const Choose = {
     [2]: 'Scissors'
 };
 
-const maxOptions = Object.keys(Choose).length - 1;
-
 function getRandomInt(min, max) {
+
+    let inclusiveMax = 1;
+    
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return Math.floor(Math.random() * (max - min + inclusiveMax)) + min;
 }
 
 //Decides what will the computer choose from Rock, Paper or Scissors
 function getComputerChoice(){
 
-    let randomNumber = getRandomInt(0, maxOptions);
-    
-    return Object.keys(Choose)[randomNumber];
+    let numRandomMin = 0
+    let numRandomMax = 2;
+
+    let randomNumber = getRandomInt(numRandomMin, numRandomMax);
+
+    return randomNumber;
 }
 
 //Prompt user to choose from Rock, Paper or Scissors
